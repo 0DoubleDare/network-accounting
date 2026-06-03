1ec04985a01da00685bc9154a80c8469e68e6083
0DoubleDare <lidernikita905@gmail.com>
2026-06-03 10:46:13 +0400

    Исправил структуру

--- app/includes/header.php
+++ app/includes/header.php
@@ -12,7 +12,7 @@ $web_root = '/' . $web_root . '/';
 <head>
     <meta charset="UTF-8">
     <title>Учёт сетевой инфраструктуры</title>
-    <link rel="stylesheet" href="<?= $web_root ?>../public/assets/css/style.css"
+    <link rel="stylesheet" href="<?= $web_root ?>public/assets/css/style.css"
     <script src="assets/js/script.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
