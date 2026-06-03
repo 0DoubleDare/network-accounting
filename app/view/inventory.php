e5dec70d8d4bf11018cd8e539b7fdca8eebe7830
Kirill <kirillzukov134@gmail.com>
2026-06-03 22:14:49 +0400

    Сделал таблицу: thead сделал синим и добавил кнопки

--- app/view/inventory.php
+++ app/view/inventory.php
@@ -19,6 +19,7 @@ if (!isset($points)) {
 <body>
 <div class="container mt-4 w-20 p-3 border border-dark" style="background-color: #eee;">
     <h1 class="text-center">Реестр оборудования</h1>
+    <a href="#" class="btn btn-success mb-3">Добавить точку</a>
     <table class="table table-bordered table-striped">
         <thead class="table-primary">
             <tr>
@@ -30,6 +31,7 @@ if (!isset($points)) {
                 <th>Проверено</th>
                 <th>Дата создания</th>
                 <th>Фото</th>
+                <th>Действие</th>
             </tr>
         </thead>
         <tbody>
@@ -49,6 +51,10 @@ if (!isset($points)) {
                 <th><?php echo $point['last_check'] ?></th>
                 <th><?php echo $point['point_created_at'] ?></th>
                 <th><?php echo $point['image_path']?></th>
+                <th>
+                    <a href="#" class="btn btn-danger">Удалить</a>
+                    <a href="#" class="btn btn-warning">Изменить</a>
+                </th>
             </tr>
             <?php endforeach; ?>
             <?php endif; ?>
