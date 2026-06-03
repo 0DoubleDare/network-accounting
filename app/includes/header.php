1cc1db5a2d04ab5109820da5e0e7f63aefb09020
0DoubleDare <lidernikita905@gmail.com>
2026-06-03 21:06:48 +0400

    Исправил таблицу инвентаря

--- app/includes/header.php
+++ app/includes/header.php
@@ -21,27 +21,27 @@ if (session_status() === PHP_SESSION_NONE) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<header style="background: white; padding: 20px 40px; border-bottom: 1px solid #c0c0c0;">
-    <div style="display: flex; justify-content: space-between; align-items: center;">
-        <a href="<?= $web_root ?>public/index.php"><h1 style="color: #333; font-size: 24px; font-weight: 600;">🍵 Network accounting</h1></a>
-        <nav style="display: flex; gap: 12px;">
-            <a href="<?= $web_root ?>app/view/registration.php"><button type="button" class="btn btn-primary">Регистрация</button></a>
-            <a href="<?= $web_root ?>app/view/login.php"><button type="button" class="btn btn-success">Войти</button></a></a>
-            <a href="<?= $web_root ?>controllers/inventorycontroller.php?action=index"><button type="button" class="btn btn-info">Инвентарь</button></a>
+<!--<header style="background: white; padding: 20px 40px; border-bottom: 1px solid #c0c0c0;">-->
+<!--    <div style="display: flex; justify-content: space-between; align-items: center;">-->
+<!--        <a href="--><?php //= $web_root ?><!--public/index.php"><h1 style="color: #333; font-size: 24px; font-weight: 600;">🍵 Network accounting</h1></a>-->
+<!--        <nav style="display: flex; gap: 12px;">-->
+<!--            <a href="--><?php //= $web_root ?><!--app/view/registration.php"><button type="button" class="btn btn-primary">Регистрация</button></a>-->
+<!--            <a href="--><?php //= $web_root ?><!--app/view/login.php"><button type="button" class="btn btn-success">Войти</button></a></a>-->
 <header class="site-header">
     <div class="header-container">
         <a href="<?= $web_root ?>public/index.php" class="site-logo">
             <h1 class="logo-title">Network accounting</h1>
         </a>
-<nav style="display: flex; gap: 12px; align-items: center">
-    <?php if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info'])): ?>
-        <span class=""><?php echo htmlspecialchars($_SESSION['user_info']['login'] ?? ''); ?></span>
-        <a href="<?= $web_root ?>controllers/logout.php"><button type="button" class="btn btn-danger">Выйти</button></a>
-    <?php else: ?>
-        <a href="<?= $web_root ?>app/view/registration.php"><button type="button" class="btn btn-primary">Регистрация</button></a>
-        <a href="<?= $web_root ?>app/view/login.php"><button type="button" class="btn btn-success">Войти</button></a>
-    <?php endif; ?>
-</nav>
+        <a href="<?= $web_root ?>controllers/inventorycontroller.php?action=index"><button type="button" class="btn btn-info">Инвентарь</button></a>
+        <nav style="display: flex; gap: 12px; align-items: center">
+            <?php if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info'])): ?>
+                <span class=""><?php echo htmlspecialchars($_SESSION['user_info']['login'] ?? ''); ?></span>
+                <a href="<?= $web_root ?>controllers/logout.php"><button type="button" class="btn btn-danger">Выйти</button></a>
+            <?php else: ?>
+                <a href="<?= $web_root ?>app/view/registration.php"><button type="button" class="btn btn-primary">Регистрация</button></a>
+                <a href="<?= $web_root ?>app/view/login.php"><button type="button" class="btn btn-success">Войти</button></a>
+            <?php endif; ?>
+        </nav>
     </div>
 </header>
 <main class="content">
