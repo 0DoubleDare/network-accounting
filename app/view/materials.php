da262a4f8555c1ddc13ec41002ad663ec55abb1f
v4432887-beep <v4431887@gmail.com>
2026-06-05 13:46:14 +0400

    добавление, редактирование, удаление, материалов

--- app/view/materials.php
+++ app/view/materials.php
@@ -2,4 +2,19 @@
 /**
  * Учёт расходников. Кабели (м.), коннекторы (шт.), розетки (шт.).
  */
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insertMaterials.php" method="post">
+        <button>Добавить материал</button><br><br>
+    </form>
+</body>
+</html>
---
+++ app/view/updateMaterials.php
@@ -0,0 +1,51 @@
+<?php
+require '../../config/db.php';
+require '../includes/functions.php';
+$id = $_GET['id'];
+$idMaterials = materialsId($pdo, $id);
+$typesId = materialTypeId($pdo, $id);
+$types = materialType($pdo);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="../../controllers/updateMaterialsControllers.php">
+    <input type="hidden" name="id" value="<?php echo $idMaterials['id']; ?>">
+    
+    <label>Название</label><br>
+        <input type="text" name="name" value="<?php echo htmlspecialchars($idMaterials['name']); ?>"><br><br>
+
+        <label>тип</label><br>
+        <select name="type">
+
+        <?php foreach($typesId as $typeId): ?>
+            <option value="<?= $typeId['id']?>" <?= $typeId['id'] == $idMaterials['type'] ? 'selected' : ''?> >
+                <?= $typeId['display_name']; ?>
+            </option>
+            <?php endforeach; ?>
+
+            <?php foreach($types as $type): ?>
+            <option value="<?= $type['id']?>"><?php echo htmlspecialchars($type['name']); ?></option>
+            <?php endforeach; ?>
+        </select><br><br>
+        
+        <label>Единица измерения</label><br>
+        <select name="unit">
+            <option value="<?= $idMaterials['id']?>"><?php echo htmlspecialchars($idMaterials['unit']);?></option>
+
+            <option value="m">m</option>
+            <option value="pcs">pcs</option>
+        </select><br><br>
+
+        <button type="submit">Редактировать</button>
+
+
+    </form>
+</body>
+</html>
