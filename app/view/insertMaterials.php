052c6f2aa34e44bc2019f10ff7f7af24500d5899
Almira <limalmiras@gmail.com>
2026-06-07 01:35:28 +0400

    Обновила стили добавления и изменения материалов

--- app/view/insertMaterials.php
+++ app/view/insertMaterials.php
@@ -6,30 +6,43 @@ $types = materialType($pdo);
 
 ?>
 
-
 <?php include '../includes/header.php'; ?>
-    </head>
-    <body>
-    <form method="post" action="../../controllers/insertMaterials.php">
-        <label>Название</label><br>
-        <input type="text" name="name"><br><br>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        <h4 class="mb-0">Добавление материала</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="../../controllers/insertMaterials.php">
+                            <div class="mb-3">
+                                <label class="form-label">Название</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
 
-        <label>тип</label><br>
-        <select name="type">
-            <?php foreach ($types as $type): ?>
-                <option value="<?= $type['id'] ?>"><?php echo htmlspecialchars($type['display_name']); ?></option>
-            <?php endforeach; ?>
-        </select><br><br>
+                            <div class="mb-3">
+                                <label class="form-label">Тип</label>
+                                <select name="type" class="form-select">
+                                    <?php foreach ($types as $type): ?>
+                                        <option value="<?= $type['id'] ?>"><?php echo htmlspecialchars($type['display_name']); ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
 
-        <label>Единица измерения</label><br>
-        <select name="unit">
-            <option value="m">m</option>
-            <option value="pcs">pcs</option>
-        </select><br><br>
+                            <div class="mb-3">
+                                <label class="form-label">Единица измерения</label>
+                                <select name="unit" class="form-select">
+                                    <option value="m">m</option>
+                                    <option value="pcs">pcs</option>
+                                </select>
+                            </div>
 
-        <button type="submit">Добавить</button>
-
-    </form>
-    </body>
-
+                            <button type="submit" class="btn btn-primary">Добавить</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php include '../includes/footer.php'; ?>
