5ec9ea5373beac689d2b6bfb01ca3ad024265813
0DoubleDare <lidernikita905@gmail.com>
2026-06-08 10:31:23 +0400

    Фикс диаграмм

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 //Подключение к БД (MySql). Используется во всех файлах, где нужен доступ к БД.
 try {
-    $pdo = new PDO('mysql:host=db;dbname=network_accounting_db', 'root', '');
-} catch(PDOException $error) {
+    $pdo = new PDO('mysql:host=localhost;dbname=network_accounting_db', 'root', '');
+} catch (PDOException $error) {
     die('Error: ' . $error->getMessage());
 }
 ?>
