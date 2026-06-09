74b120cbcb777f6ae3e266d2b09c21fe0ead1bb9
Ярослав <aroslavlancetov@gmail.com>
2026-06-09 10:26:44 +0400

    inventory.php

--- app/view/inventory/inventory.php
+++ app/view/inventory/inventory.php
@@ -145,12 +145,12 @@ if (!isset($points)) {
                                     ?>
                                 </td>
                                 <td class="pe-4">
-                                    <a href="../defects/defects_view.php?point_id=<?= $point['id'] ?>">Подробнее</a>
+                                    <a type="button" class="btn btn-outline-primary ms-2" href="../defects/defects_view.php?point_id=<?= $point['id'] ?>">Подробнее</a>
                                     <?php if (isset($_SESSION['user_info']) && !empty($_SESSION['user_info'])): ?>
                                         <a href="../../app/view/inventory/update_network_point.php?id=<?php echo htmlspecialchars($point['id']); ?>"
-                                        class="btn btn-outline-secondary btn-sm ms-2">Изменить</a>
+                                        class="btn btn-outline-secondary  ms-2">Изменить</a>
                                         <a href="./delete_network_point.php?id=<?php echo htmlspecialchars($point['id']); ?>"
-                                        class="btn btn-outline-danger btn-sm ms-2" 
+                                        class="btn btn-outline-danger  ms-2" 
                                         onclick="return confirm('Удалить точку?')">Удалить</a>
                                     <?php else: ?>
                                         <button class="btn btn-outline-secondary btn-sm ms-2" disabled 
