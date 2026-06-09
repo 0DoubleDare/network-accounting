88cf2de9e75d55d629002369b0c68fec5e3042f6
v4432887-beep <v4431887@gmail.com>
2026-06-09 12:31:11 +0400

    проверено добавление дефекта и наличие кнопки

--- controllers/defects/insert_defects.php
+++ controllers/defects/insert_defects.php
@@ -1,10 +1,8 @@
 <?php
-
 require '../../config/db.php';
 require '../../app/includes/functions.php';
 
 insert_defects($pdo, $_POST['point_id'], $_POST['category'], $_POST['severity'], $_POST['description'], $_POST['status'],
  $_POST['created_by'], $_FILES['image_name']);
-
 
 header('Location: ../../app/view/defects/defects.php');
